68034ca7b527d60918b8b3a545366abf43d65b76
benui <ben@benui.ca>
2022-07-28 18:24:34 -0700

    Split up logic to make subclassing easier

--- Source/BYGYouTrackFiller/Public/BYGYouTrackFiller.h
+++ Source/BYGYouTrackFiller/Public/BYGYouTrackFiller.h
@@ -15,5 +15,13 @@ public:
 	virtual bool FillAndShowTicket(const FBYGYouTrackTicketData& Data, const TArray<FString>& OtherPathsToOpen = TArray<FString>());
 	
 protected:
+	// Get the list of paths to run through FPlatformMisc::OnExec
+	// Override this to add other directories that you want to open and show when filling a new ticket
+	virtual void GetPathsToOpen(const FBYGYouTrackTicketData& Data, TArray<FString>& Paths);
+	
+	// Customize how the URL is built
+	virtual FString GetYouTrackURL(const FBYGYouTrackTicketData& Data);
+
+	// Merge with defaults set in Project Settings
 	virtual FBYGYouTrackTicketData Merge(const FBYGYouTrackTicketData& DefaultsData, const FBYGYouTrackTicketData& CustomData);
 };
