523df780212842d055166619713c61eef0e20c2d
benui <ben@benui.ca>
2022-07-28 17:58:47 -0700

    Now works on 4.25

--- Source/BYGYouTrackFillerEditor/Private/BYGYouTrackFillerStyle.cpp
+++ Source/BYGYouTrackFillerEditor/Private/BYGYouTrackFillerStyle.cpp
@@ -5,7 +5,12 @@
 #include "Styling/SlateStyleRegistry.h"
 #include "Slate/SlateGameResources.h"
 #include "Interfaces/IPluginManager.h"
+
+#if ENGINE_MAJOR_VERSION >= 5
 #include "Styling/SlateStyleMacros.h"
+#else
+#define IMAGE_BRUSH( RelativePath, ... ) FSlateImageBrush( RootToContentDir( RelativePath, TEXT(".png") ), __VA_ARGS__ )
+#endif
 
 #define RootToContentDir Style->RootToContentDir
 
@@ -36,13 +41,18 @@ FName FBYGYouTrackFillerStyle::GetStyleSetName()
 
 const FVector2D Icon16x16(16.0f, 16.0f);
 const FVector2D Icon20x20(20.0f, 20.0f);
+const FVector2D Icon40x40(40.0f, 40.0f);
 
 TSharedRef< FSlateStyleSet > FBYGYouTrackFillerStyle::Create()
 {
 	TSharedRef< FSlateStyleSet > Style = MakeShareable(new FSlateStyleSet("BYGYouTrackFillerStyle"));
 	Style->SetContentRoot(IPluginManager::Get().FindPlugin("BYGYouTrackFiller")->GetBaseDir() / TEXT("Resources"));
 
+#if ENGINE_MAJOR_VERSION >= 5
 	Style->Set("BYGYouTrackFiller.FillAndShowYouTrack", new IMAGE_BRUSH_SVG(TEXT("YouTrackFillerButtonIcon"), Icon20x20));
+#else
+	Style->Set("BYGYouTrackFiller.FillAndShowYouTrack", new IMAGE_BRUSH(TEXT("YouTrackFillerButtonIcon"), Icon40x40));
+#endif
 	return Style;
 }
 
